e2a1d74295de65d8b3f7335068b225f0ba59ec3e
Nick Bennett <nick@nickbenn.com>
2020-05-28 11:06:37 -0600

    Create Shoe (incl. shuffle) & ShoeTester.

--- src/edu/cnm/deepdive/Suit.java
+++ src/edu/cnm/deepdive/Suit.java
@@ -1,9 +1,16 @@
 package edu.cnm.deepdive;
 
+/**
+ * Encapsulates the suits of standard playing cards. Each instance can return (using the
+ * {@link #symbol()} method) the Unicode symbol for that suit.
+ */
 public enum Suit {
 
   CLUBS, DIAMONDS, HEARTS, SPADES;
 
+  /**
+   * Returns the <em>Unicode</em> symbol for the current instance.
+   */
   public char symbol() {
     switch (this) {
       case CLUBS:
